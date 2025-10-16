32d10ef69a442d3c1aa3ba73e9675c9abeb19191
Vivek Mishra <vivekmishra8009@gmail.com>
2025-10-16 22:20:42 +0530

    inti updated project

--- App.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import SplashScreen from './src/screens/SplashScreen';
-
-const App = () => {
-  return <SplashScreen />;
-};
-
-export default App;
--- src/constants/colors.ts
+++
@@ -1,9 +0,0 @@
-export default {
-    primary: '#4e9cff',
-    secondary: '#ffffff',
-    text: '#333333',
-    border: '#d3d3d3',
-    error: '#ff4d4d',
-    placeholder: '#999999',
-  };
-  
